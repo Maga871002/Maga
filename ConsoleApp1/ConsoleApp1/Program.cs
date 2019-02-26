a8793f67f7d790f5f10b376dbcd858140ab22f96
Maga <ya.nastya-medvedeva@yandex.by>
2019-02-26 14:54:32 +0300

    commit2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    public class dog
+    {
+
+        public string Nickname { get; set; }
+        public string Breed { get; set; }
+        public double Age { get; set; }
+
+        public dog()
+        {
+            Nickname = "";
+            Breed = "";
+            Age = 0;
+        }
+
+        public void vvod()
+        {
+            Console.WriteLine("Добавление новой собаки => ");
+            Console.Write("Кличка: ");
+            Nickname = Console.ReadLine();
+            Console.Write("Порода: ");
+            Breed = Console.ReadLine();
+            Console.Write("Возраст: ");
+            Age = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void vivod()
+        {
+            Console.WriteLine("Кличка: {0}, Порода: {1}, Возраст {2}", Nickname, Breed, Age);
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
